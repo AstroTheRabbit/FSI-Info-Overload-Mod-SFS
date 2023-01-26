e6ef4c7c19bf9693714f6bd84162b9d748857ce1
pixelgaming579 <jack.pryde@outlook.com>
2023-01-26 15:39:59 +1100

    Added Icon

--- InfoOverload/Main.cs
+++ InfoOverload/Main.cs
@@ -29,7 +29,7 @@ namespace InfoOverload
         public override string MinimumGameVersionNecessary => "1.5.9.8";
         public override string ModVersion => "1.0";
         public override string Description => "Visualises colliders, ranges and other invisible/technical stuff. Made for Fusion Space Industries.";
-        public override string IconLink => "";
+        public override string IconLink => "https://i.imgur.com/D6heH5y.png";
         public override Dictionary<string, string> Dependencies { get; } = new Dictionary<string, string> { { "UITools", "1.1.1" } };
 
         public override void Load()
