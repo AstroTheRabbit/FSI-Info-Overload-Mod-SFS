267a8b2e1c507c841eab595be68ec30ce1315c40
VerdiX094 <neoziomek5@gmail.com>
2024-12-07 00:33:23 +0100

    Create a map sort-of GLDrawer class

---
+++ InfoOverload/MapVisualHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using SFS.World.Maps;
+using SFS.WorldBase;
+using UnityEngine;
+
+namespace InfoOverload
+{
+    public static class MapVisualHelper
+    {
+        public static void DrawLine(Double2[] points, Planet planet, Color color)
+        {
+            var conv = new Vector3[points.Length];
+
+            for (var i = 0; i < points.Length; i++)
+            {
+                // Parentheses to avoid unnecessary additional precision loss
+                conv[i] = new Vector3((float) (points[i].x / 1000), (float) (points[i].y / 1000));
+            }
+            
+            Map.solidLine.DrawLine(conv, planet, color, color);
+        }
+    }
+}
